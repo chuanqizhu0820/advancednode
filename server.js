fd38537161abbdf216eb06aec0890c4c28afc11f
Chuanqi Zhu <chuanqizhu1991@outlook.com>
2021-12-09 17:56:49 +0800

    Connect to database

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-let express = require('express');
-const session = require('express-session')
+let express = require('express')
+let session = require('express-session')
+let passport = require('passport')
+let mongo = require('mongodb').MongoClient;
 let app = express();
 
 app.set('view engine', 'pug')
@@ -10,10 +12,21 @@ app.use(session({
   resave: true,
   saveUninitialized: true,
 }))
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.get('/', (req, res)=>{
-    console.log(req.session)
-    res.render('homepage', {message: "this is page to learn advanced node"})
+let uri = "mongodb+srv://dbUser:dbUserpw@cluster0.f1gxa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongo.connect(uri, (err, db)=>{
+    if(err){
+        console.log(err);
+    }else{
+        app.listen(3000);
+
+        app.get('/', (req, res)=>{
+        console.log(req.session)
+        res.render('homepage', {message: "this is page to learn advanced node"})
+})
+    }
 })
 
-app.listen(3000);
