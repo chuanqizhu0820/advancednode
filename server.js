2d0856237185bc666d8e57b34ba87afcf1275279
Chuanqi Zhu <chuanqizhu1991@outlook.com>
2021-12-09 16:43:59 +0800

    Pass variables to template

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 let express = require('express');
 let app = express();
-app.listen(3000);
 
 app.set('view engine', 'pug')
 app.set('views', './pages')
 
 app.get('/', (req, res)=>{
-    res.render('homepage')
+    res.render('homepage', {message: "this is page to learn advanced node"})
 })
+
+app.listen(3000);
