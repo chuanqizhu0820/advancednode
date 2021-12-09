69c5b8628dae7de14a9a763a2a1620aa23672c5b
Chuanqi Zhu <chuanqizhu1991@outlook.com>
2021-12-09 17:03:34 +0800

    Set up express session

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 let express = require('express');
+const session = require('express-session')
 let app = express();
 
 app.set('view engine', 'pug')
 app.set('views', './pages')
 
+app.use(session({
+  secret: 'keyboard-cat',
+  resave: true,
+  saveUninitialized: true,
+}))
+
 app.get('/', (req, res)=>{
+    console.log(req.session)
     res.render('homepage', {message: "this is page to learn advanced node"})
 })
 
