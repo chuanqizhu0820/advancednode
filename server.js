df5764bfdd5f674b21e41394d910c17cbfcf8b19
Chuanqi Zhu <chuanqizhu1991@outlook.com>
2021-12-10 12:01:32 +0800

    Add form and make app able to receive post request

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 let express = require('express')
 let session = require('express-session')
 let passport = require('passport')
-let mongo = require('mongodb').MongoClient;
+let client = require('mongodb').MongoClient;
 let app = express();
 
 app.set('view engine', 'pug')
 app.set('views', './pages')
+
+app.use(express.urlencoded({
+  extended: true
+}))
 
 app.use(session({
   secret: 'keyboard-cat',
@@ -17,16 +21,20 @@ app.use(passport.session())
 
 let uri = "mongodb+srv://dbUser:dbUserpw@cluster0.f1gxa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-mongo.connect(uri, (err, db)=>{
+client.connect(uri, (err, db)=>{
     if(err){
         console.log(err);
     }else{
-        app.listen(3000);
+        app.listen(3000, ()=>{
+            console.log('your app is listening to the port 3000')
+        });
 
         app.get('/', (req, res)=>{
-        console.log(req.session)
-        res.render('homepage', {message: "this is page to learn advanced node"})
-})
+        // console.log(req.session)
+        res.render('homepage', {message: "this is page to learn advanced node"})});
+
+        app.post('/login', (req,res)=>{
+         console.log(req.body)
+        })
     }
 })
-
